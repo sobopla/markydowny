e9ac08e37728b48ac2e02d68826f1da3237a8716
sobopla <sobopla@gmail.com>
2017-08-15 15:48:35 -0500

    Release 1 MVC solution

--- js/application.js
+++ js/application.js
@@ -1,18 +1,33 @@
-function MarkdownRenderer() {
-  this.text = "Z";
+function MarkdownModel() {
 }
 
-MarkdownRenderer.prototype.transform = function(theMarkupText) {
+MarkdownModel.prototype.transform = function(theMarkupText) {
 // must do double-star before single-star
   return theMarkupText.replace(/\*\*(.*)\*\*/g, "<b>$1</b>")
                       .replace(/\*(.*)\*/g, "<i>$1</i>")
                       .replace(/_(.*)_/g, "<i>$1</i>");
 }
 
-var renderer = new MarkdownRenderer;
+function MarkdownView() {
+}
+
+MarkdownView.prototype.getMarkdownText = function() {
+  return $(".userinput").val();
+}
+
+MarkdownView.prototype.setHTML = function(theHtmlVersion) {
+  $("#preview").html(theHtmlVersion);
+}
+
+function MarkdownController(model, view) {
+  $(".userinput").keyup(function(){
+    view.setHTML(model.transform(view.getMarkdownText()));
+  });
+}
 
 $(document).ready(function(){
-  $(".markdown").keyup(function(){
-    $("#preview").html(renderer.transform($(".markdown").val()));
-  });
+  var model      = new MarkdownModel;
+  var view       = new MarkdownView;
+  var controller = new MarkdownController(model, view);
 });
+
