8a50aeffa18a721679d7c165be4d16556bafa718
sobopla <sobopla@gmail.com>
2017-08-15 12:48:28 -0500

    bold and alternate italic also working

--- js/application.js
+++ js/application.js
@@ -3,7 +3,10 @@ function MarkdownRenderer() {
 }
 
 MarkdownRenderer.prototype.transform = function(theMarkupText) {
-  return theMarkupText.replace(/_(.*)_/g, "<i>$1</i>");
+// must do double-star before single-star
+  return theMarkupText.replace(/\*\*(.*)\*\*/g, "<b>$1</b>")
+                      .replace(/\*(.*)\*/g, "<i>$1</i>")
+                      .replace(/_(.*)_/g, "<i>$1</i>");
 }
 
 var renderer = new MarkdownRenderer;
